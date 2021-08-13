ab69b24743332e1d0a11abc263ce26fd1601be3b
Arbi Wiranata <arbiwiranata48@gmail.com>
2021-08-13 11:23:40 +0700

    FIX: SQL Compiler Jadwal Kerja

--- migration/20210809.Export-Import-Rev/20210809.01.Jadwal-Kerja.sql
+++ migration/20210809.Export-Import-Rev/20210809.01.Jadwal-Kerja.sql
@@ -12,7 +12,7 @@ BEGIN
     LEFT OUTER JOIN sc_mst.regu_opr b ON a.kdregu = b.kdregu
     LEFT OUTER JOIN sc_mst.karyawan c ON c.nik = b.nik
     LEFT OUTER JOIN sc_mst.jabatan e ON c.jabatan = e.kdjabatan AND e.kddept = c.bag_dept AND e.kdsubdept = c.subbag_dept
-    WHERE a.nik = new.nik AND tgl = new.tgl
+    WHERE a.kdregu = new.kdregu AND tgl = new.tgl
     ON CONFLICT (nik, tgl)
     DO NOTHING;
 
@@ -31,7 +31,7 @@ DECLARE
 
 BEGIN
 	DELETE FROM sc_trx.dtljadwalkerja
-	WHERE nik = new.nik and tgl = new.tgl;
+	WHERE kdregu = new.kdregu and tgl = new.tgl;
 
 	INSERT INTO sc_trx.dtljadwalkerja (nik, shift, tgl, kdjamkerja, kdregu, kdmesin)
     SELECT b.nik, e.shift, tgl, kodejamkerja, a.kdregu, kdmesin
@@ -40,7 +40,7 @@ BEGIN
     LEFT OUTER JOIN sc_mst.regu_opr b ON a.kdregu = b.kdregu
     LEFT OUTER JOIN sc_mst.karyawan c ON c.nik = b.nik
     LEFT OUTER JOIN sc_mst.jabatan e ON c.jabatan = e.kdjabatan AND e.kddept = c.bag_dept AND e.kdsubdept = c.subbag_dept
-    WHERE a.nik = new.nik AND tgl = new.tgl
+    WHERE a.kdregu = new.kdregu AND tgl = new.tgl
 	ON CONFLICT (nik, tgl)
     DO NOTHING;
 
@@ -59,7 +59,7 @@ DECLARE
 
 BEGIN
 	DELETE FROM sc_trx.dtljadwalkerja
-	WHERE nik = old.nik AND tgl = old.tgl;
+	WHERE kdregu = old.kdregu AND tgl = old.tgl;
 
     RETURN new;
 END;
