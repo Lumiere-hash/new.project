85b466351f0c50b57dd002d0b194da657d27048b
nusantaragroup <nusagroup.it.div@gmail.com>
2023-09-22 15:03:35 +0700

    Add: Generate UM Otomatis

--- application/migrations/sql/00019.20230921.20.Revisi-Tunjangan-Tidak-Tetap/+up/003.alter.fuction.sc_trx.tr_dinas_after_update.sql
+++ application/migrations/sql/00019.20230921.20.Revisi-Tunjangan-Tidak-Tetap/+up/003.alter.fuction.sc_trx.tr_dinas_after_update.sql
@@ -195,6 +195,67 @@ BEGIN
                             AND nodok = new.nodok
                             and nik = new.nik;
     END IF;
+	
+	IF ( NEW.status = 'C' and OLD.status <> 'C' ) THEN
+        update sc_trx.approvals_system set status='C',asstatus='C' where docno = new.nodok;
+        INSERT INTO sc_his.dinas (
+            nik,
+            nodok,
+            tgl_dok,
+            nmatasan,
+            tgl_mulai,
+            jam_mulai,
+            tgl_selesai,
+            jam_selesai,
+            status,
+            keperluan,
+            tujuan_kota,
+            input_date,
+            input_by,
+            approval_date,
+            approval_by,
+            delete_date,
+            delete_by,
+            update_by,
+            update_date,
+            cancel_by,
+            cancel_date,
+            kdkategori,
+            transportasi,
+            tipe_transportasi,
+            jenis_tujuan,
+            no_telp
+        ) SELECT
+              nik,
+              nodok,
+              tgl_dok,
+              nmatasan,
+              tgl_mulai,
+              jam_mulai,
+              tgl_selesai,
+              jam_selesai,
+              status,
+              keperluan,
+              tujuan_kota,
+              input_date,
+              input_by,
+              approval_date,
+              approval_by,
+              delete_date,
+              delete_by,
+              update_by,
+              update_date,
+              cancel_by,
+              cancel_date,
+              kdkategori,
+              transportasi,
+              tipe_transportasi,
+              jenis_tujuan,
+              no_telp
+        from sc_trx.dinas where TRUE
+                            AND nodok = new.nodok
+                            and nik = new.nik;
+    END IF;
 
     IF ( NEW.status = 'A' and OLD.status <> 'A' ) THEN
         perform sc_trx.pr_capture_approvals_system();
