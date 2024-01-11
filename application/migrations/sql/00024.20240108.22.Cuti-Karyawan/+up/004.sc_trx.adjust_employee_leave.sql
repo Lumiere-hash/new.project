0bcdafd4f7ab7bdbf7c545ce4f73295797d083c6
Rizal <rkm.kurniawan@gmail.com>
2024-01-11 11:48:13 +0700

    FIX: Modul Cuti
    ->> Penyesuaian trigger cuti ulang tahun

--- application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/004.sc_trx.adjust_employee_leave.sql
+++ application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/004.sc_trx.adjust_employee_leave.sql
@@ -1,8 +1,10 @@
+DROP FUNCTION IF EXISTS sc_trx.adjust_employee_leave();
 CREATE OR REPLACE FUNCTION sc_trx.adjust_employee_leave()
-    RETURNS INT AS $$
+    RETURNS varchar AS $$
 DECLARE
     --author:: RKM
     done BOOLEAN DEFAULT FALSE;
+    last_check varchar;
     emp_id varchar;
     emp_join_date DATE;
     emp_entry_date_limit DATE;
@@ -13,10 +15,12 @@ DECLARE
     count_now INT;
     -- Declare a cursor to iterate through employees
 BEGIN
+
     IF NOT EXISTS (SELECT FROM public.function_log WHERE function_name = 'sc_trx.adjust_employee_leave' AND actived) THEN
         emp_entry_date_limit := coalesce(value1, '2022-01-15')::date AS value1 FROM sc_mst.option WHERE kdoption = 'EMPLOYEE:ENTRY:DATE' AND group_option = 'CUTI';
         count_now = 0;
         count_selected := count(*) FROM sc_mst.karyawan WHERE TRUE
+
                                                           AND statuskepegawaian <> 'KO'
                                                           AND COALESCE(UPPER(grouppenggajian), '') != 'P0'
                                                           AND (
@@ -28,14 +32,15 @@ BEGIN
                 );
         -- Start looping through employees
         FOR emp_record IN SELECT nik, tglmasukkerja, sisacuti FROM sc_mst.karyawan WHERE TRUE
-             AND statuskepegawaian <> 'KO'
-             AND COALESCE(UPPER(grouppenggajian), '') != 'P0'
-             AND (
-                    tglmasukkerja <= TO_CHAR(now()::DATE - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
-                    tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '1 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
-                    tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '2 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
-                    tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '3 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
-                    tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '4 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE
+
+                                                                                     AND statuskepegawaian <> 'KO'
+                                                                                     AND COALESCE(UPPER(grouppenggajian), '') != 'P0'
+                                                                                     AND (
+                        tglmasukkerja <= TO_CHAR(now()::DATE - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
+                        tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '1 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
+                        tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '2 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
+                        tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '3 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE OR
+                        tglmasukkerja <= TO_CHAR((now()::DATE + INTERVAL '4 DAY') - INTERVAL '1 YEAR', 'YYYY-MM-DD')::DATE
                 ) LOOP
                 emp_id := emp_record.nik;
                 emp_join_date := emp_record.tglmasukkerja;
@@ -45,13 +50,8 @@ BEGIN
                 IF TO_DATE(concat(to_char(now(),'YYYY'),'-',to_char(emp_join_date,'MM-DD')), 'YYYY-MM-DD') <= TO_DATE(concat(to_char(now(),'YYYY'),'-',to_char(emp_join_date,'MM'),'-15'), 'YYYY-MM-DD') THEN
                     emp_leave_adjusment = emp_leave_adjusment;
                 ELSE
-                    IF emp_leave_adjusment > 0 THEN
-                        emp_leave_adjusment = emp_leave_adjusment - 1;
-                    end if;
+                    emp_leave_adjusment = emp_leave_adjusment + 1;
                 end if;
-                IF emp_leave_adjusment = 0 THEN
-                    emp_leave_adjusment := 12;
-                END IF;
                 -- Check the condition and perform actions accordingly
                 IF emp_join_date <= emp_entry_date_limit THEN
                     -- If tgl_masukkerja is less than or equal to '2022-01-15'
@@ -68,7 +68,7 @@ BEGIN
                         status
                     ) VALUES (
                                  emp_id,
-                                 TO_CHAR(now(), 'YYYY-MM-DD 00:00:06')::TIMESTAMP ,
+                                 TO_CHAR(now(), 'YYYY-01-01 00:00:06')::TIMESTAMP ,
                                  concat('ADJ',to_char(now(), 'YYYY')),
                                  emp_leave_adjusment,
                                  0,
@@ -76,6 +76,8 @@ BEGIN
                                  'IN',
                                  'ADJUSTMENT'
                              );
+
+
                     -- Add your logic here for condition 'a'
                 ELSEIF emp_join_date > emp_entry_date_limit THEN
                     -- If tgl_masukkerja is greater than '2022-01-15'
@@ -92,13 +94,13 @@ BEGIN
                         status
                     ) VALUES (
                                  emp_id,
-                                 TO_CHAR(now(), 'YYYY-MM-DD 00:00:06')::TIMESTAMP ,
+                                 TO_CHAR(now(), 'YYYY-01-01 00:00:06')::TIMESTAMP ,
                                  concat('ADJ',to_char(now(), 'YYYY')),
                                  emp_leave_adjusment,
                                  0,
                                  emp_leave_last + emp_leave_adjusment,
                                  'IN',
-                                 'TRIAL'
+                                 'ADJUSTMENT'
                              );
                     -- Add your logic here for condition 'b'
                 END IF;
@@ -107,23 +109,23 @@ BEGIN
                 -- Add your common logic here for adjusting leave
                 count_now := count_now + 1;
             END LOOP;
-            IF count_now = count_selected then
-                insert INTO "public".function_log(
-                    function_name,
-                    actived,
-                    actived_by,
-                    actived_date
-                ) VALUES (
-                             'sc_trx.adjust_employee_leave',
-                             true,
-                             'SYSTEM',
-                             NOW()
-                         );
-                RETURN 11; -- Replace with appropriate return value
-            else
-                rollback ;
-                RETURN 0 ;
-            end if;
+        IF count_now = count_selected then
+            insert INTO "public".function_log(
+                function_name,
+                actived,
+                actived_by,
+                actived_date
+            ) VALUES (
+                         'sc_trx.adjust_employee_leave',
+                         true,
+                         'SYSTEM',
+                         NOW()
+                     );
+            RETURN 1 ; -- Replace with appropriate return value
+        else
+--             rollback ;
+            RETURN 0 ;
+        end if;
     ELSE
         RETURN 0;
     END IF;
--- application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/005.sc_trx.deduction_employee_leave.sql
+++ application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/005.sc_trx.deduction_employee_leave.sql
@@ -1,26 +1,28 @@
 CREATE OR REPLACE FUNCTION sc_trx.deduction_employee_leave()
-        RETURNS INT AS $$
-    DECLARE
-        --author:: RKM
-        done BOOLEAN DEFAULT FALSE;
-        emp_id varchar;
-        emp_join_date DATE;
-        emp_entry_date_limit DATE;
-        emp_leave_last INT;
-        emp_leave_deduction INT;
-        emp_record RECORD;
-        count_selected INT;
-        count_now INT;
-        -- Declare a cursor to iterate through employees
-    BEGIN
-        count_now = 0;
-        emp_entry_date_limit := coalesce(value1, '2022-01-15')::date AS value1 FROM sc_mst.option WHERE kdoption = 'EMPLOYEE:ENTRY:DATE' AND group_option = 'CUTI';
-        count_selected := count(*) FROM sc_mst.karyawan WHERE TRUE
-                          AND statuskepegawaian <> 'KO'
-                          AND COALESCE(UPPER(grouppenggajian), '') != 'P0';
-        IF NOT EXISTS (SELECT FROM public.function_log WHERE function_name = 'sc_trx.deduction_employee_leave' AND actived) THEN
+    RETURNS INT AS $$
+DECLARE
+    --author:: RKM
+    done BOOLEAN DEFAULT FALSE;
+    emp_id varchar;
+    emp_join_date DATE;
+    emp_entry_date_limit DATE;
+    emp_leave_last INT;
+    emp_leave_deduction INT;
+    emp_record RECORD;
+    count_selected INT;
+    count_now INT;
+    -- Declare a cursor to iterate through employees
+BEGIN
+    count_now = 0;
+    emp_entry_date_limit := coalesce(value1, '2022-01-15')::date AS value1 FROM sc_mst.option WHERE kdoption = 'EMPLOYEE:ENTRY:DATE' AND group_option = 'CUTI';
+    count_selected := count(*) FROM sc_mst.karyawan WHERE TRUE
+
+                                                      AND statuskepegawaian <> 'KO'
+                                                      AND COALESCE(UPPER(grouppenggajian), '') != 'P0';
+    IF NOT EXISTS (SELECT FROM public.function_log WHERE function_name = 'sc_trx.deduction_employee_leave' AND actived) THEN
         -- Start looping through employees
         FOR emp_record IN SELECT nik, tglmasukkerja, sisacuti FROM sc_mst.karyawan WHERE TRUE
+
                                                                                      AND statuskepegawaian <> 'KO'
                                                                                      AND COALESCE(UPPER(grouppenggajian), '') != 'P0'
             LOOP
@@ -46,13 +48,13 @@ CREATE OR REPLACE FUNCTION sc_trx.deduction_employee_leave()
                         status
                     ) VALUES (
                                  emp_id,
-                                 TO_CHAR(now(), 'YYYY-MM-DD 00:00:07')::TIMESTAMP ,
+                                 TO_CHAR(now(), 'YYYY-03-01 00:00:07')::TIMESTAMP ,
                                  concat('HGS',to_char(now(), 'YYYY')),
                                  0,
                                  emp_leave_deduction,
                                  emp_leave_last - emp_leave_deduction,
                                  'HGS',
-                                 'HANGUS'
+                                 'HANGUS CUTI '||TO_CHAR(now() - INTERVAL '1 YEAR', 'YYYY')
                              );
                     -- Add your logic here for condition 'a'
 
@@ -62,25 +64,25 @@ CREATE OR REPLACE FUNCTION sc_trx.deduction_employee_leave()
                 -- Add your common logic here for adjusting leave
                 count_now := count_now + 1;
             END LOOP;
-            IF count_now = count_selected then
-                insert INTO "public".function_log(
-                    function_name,
-                    actived,
-                    actived_by,
-                    actived_date
-                ) VALUES (
-                             'sc_trx.deduction_employee_leave',
-                             true,
-                             'SYSTEM',
-                             NOW()
-                         );
-                RETURN 11; -- Replace with appropriate return value
-            else
-                rollback ;
-                RETURN 0 ;
-            end if;
+        IF count_now = count_selected then
+            insert INTO "public".function_log(
+                function_name,
+                actived,
+                actived_by,
+                actived_date
+            ) VALUES (
+                         'sc_trx.deduction_employee_leave',
+                         true,
+                         'SYSTEM',
+                         NOW()
+                     );
+            RETURN 11; -- Replace with appropriate return value
+        else
+            rollback ;
+            RETURN 0 ;
+        end if;
     ELSE
-            RETURN 0;
-            END IF;
-    END;
-    $$ LANGUAGE plpgsql;
+        RETURN 0;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
