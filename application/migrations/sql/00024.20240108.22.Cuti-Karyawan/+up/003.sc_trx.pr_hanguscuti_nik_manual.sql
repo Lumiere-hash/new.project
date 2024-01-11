6b25910a62ace621386b4639fd7540fcae8dd5f9
Rizal <rkm.kurniawan@gmail.com>
2024-01-11 14:54:06 +0700

    FIX: Modul Cuti
    ->> Penyesuaian trigger cuti ulang tahun(2)

--- application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/003.sc_trx.pr_hanguscuti_nik_manual.sql
+++ application/migrations/sql/00024.20240108.22.Cuti-Karyawan/+up/003.sc_trx.pr_hanguscuti_nik_manual.sql
@@ -6,9 +6,13 @@ AS $function$
     --update by : RKM 09/01/2024 => PENYESUAIAN CUTI ULANG TAHUN--
 DECLARE vr_sisacuti integer;
     DECLARE vr_dokumen character(12);
+    DECLARE vr_check_adjustment INT;
+    DECLARE last_year_balance INT;
+    DECLARE adjusment_balance INT;
     --DECLARE vr_out integer;
 BEGIN
     vr_dokumen:='HGS'||to_char(vr_tgl,'yyyymm');
+    vr_check_adjustment := COUNT(*) FROM sc_trx.cuti_blc WHERE nik = vr_nik AND no_dokumen = 'ADJ'|| TO_CHAR(vr_tgl, 'YYYY') AND doctype = 'IN';
     --vr_sisacuti:=sisacuti from sc_mst.karyawan where nik=nike;
     vr_sisacuti := coalesce(sisacuti, 0)
                    from sc_trx.cuti_blc a,
@@ -32,10 +36,46 @@ BEGIN
                      and a.tanggal = b.tanggal
                      and a.no_dokumen = b.no_dokumen
                      and a.doctype = b.doctype;
-
+    adjusment_balance := coalesce(in_cuti, 0) as adjusment from sc_trx.cuti_blc where TRUE
+                                                                                  AND nik = vr_nik
+                                                                                  AND no_dokumen = 'ADJ2024';
+    last_year_balance := coalesce(last_year_balanced, 0) - coalesce(out_balance, 0) as balance
+                         from (select SUM(out_cuti) over (partition by nik) as out_balance,
+                                      nik
+                               from sc_trx.cuti_blc a
+                               where TRUE
+                                 AND nik = vr_nik
+                                 and tanggal <= vr_tgl
+                                 AND tanggal >= '2024-01-01'::date
+                               ORDER BY tanggal DESC
+                               LIMIt 1) aa
+                                  left join lateral (
+                             select coalesce(sisacuti, 0) - coalesce(in_cuti, 0) AS last_year_balanced,
+                                    nik
+                             from sc_trx.cuti_blc a
+                             where TRUE
+                               AND nik = vr_nik
+                               AND no_dokumen = 'ADJ2024'
+                             ORDER BY tanggal DESC
+                             LIMIt 1
+                             ) bb ON aa.nik = bb.nik;
     IF (vr_sisacuti>0) THEN --cek global jika tak mendapat cuti/cuti minus
-        insert into sc_trx.cuti_blc values
-            (vr_nik,cast(to_char(vr_tgl,'yyyy-mm-dd 00:02:02')as timestamp),vr_dokumen,0,vr_sisacuti,0,'HGS','HANGUS');
+        IF (to_char(vr_tgl,'YYYYMMDD') < to_char('2024-03-01'::date,'YYYYMMDD')) THEN
+            insert into sc_trx.cuti_blc values(
+                vr_nik,
+                cast(to_char(vr_tgl,'yyyy-mm-dd 00:02:02')as timestamp),
+                vr_dokumen,
+                0,
+                adjusment_balance,
+                (select case when (adjusment_balance - vr_sisacuti) > 0 THEN adjusment_balance - vr_sisacuti ELSE 0 END AS balanced ),
+                'HGS',
+                'HANGUS ADJUSTMENT'
+            );
+        ELSE
+            insert into sc_trx.cuti_blc values
+                (vr_nik,cast(to_char(vr_tgl,'yyyy-mm-dd 00:02:02')as timestamp),vr_dokumen,0,vr_sisacuti,0,'HGS','HANGUS');
+        end if;
+
     END IF;
     RETURN;
 END;
