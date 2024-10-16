94ecde6a48ae0d6b2deae064407b0f473be0d107
Andika Kurniawan <68938620+BedMan95@users.noreply.github.com>
2024-10-16 13:53:06 +0700

    fix: approve spk lv1, spvga

--- application/modules/ga/models/m_inventaris.php
+++ application/modules/ga/models/m_inventaris.php
@@ -704,7 +704,7 @@ class M_inventaris extends CI_Model
             $superior1 = trim($spk->row()->nik_atasan);
             $superior2 = trim($spk->row()->nik_atasan2);
 
-            // $isSPV = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'C'))->num_rows() > 0;
+            $isSPVGA = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'C', 'kdsubdept' => 'HRGA'))->num_rows() > 0;
             // $isMGR = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'B'))->num_rows() > 0;
             $isRSM = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'B', 'jabatan' => 'RSM'))->num_rows() > 0;
             $isGM = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'B', 'jabatan' => 'GMN'))->num_rows() > 0;
@@ -712,7 +712,7 @@ class M_inventaris extends CI_Model
             $isDIR = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'A'))->num_rows() > 0;
 
             $statusses = array(
-                $kode . '1' => $superior1 == $this->session->userdata('nik'),
+                $kode . '1' => $isSPVGA,
                 $kode . '2' => $superior2 == $this->session->userdata('nik'),
                 $kode . '3' => $isRSM,
                 $kode . '4' => $isGM,
