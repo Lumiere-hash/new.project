be9cce7c154fd872bd1e782c5559634b71f2218a
Andika Kurniawan <68938620+BedMan95@users.noreply.github.com>
2024-10-19 08:03:56 +0700

    refix

--- application/modules/ga/models/m_inventaris.php
+++ application/modules/ga/models/m_inventaris.php
@@ -712,8 +712,8 @@ class M_inventaris extends CI_Model
             $isDIR = $this->db->get_where('sc_mst.karyawan', array('nik' => $this->session->userdata('nik'), 'lvl_jabatan' => 'A'))->num_rows() > 0;
 
             $statusses = array(
-                $kode . '1' => $isSPVGA,
-                $kode . '2' => $superior2 == $this->session->userdata('nik'),
+                $kode . '1' => $superior1 == $this->session->userdata('nik'),
+                $kode . '2' => $isSPVGA,
                 $kode . '3' => $isRSM,
                 $kode . '4' => $isGM,
                 $kode . '5' => $isMGRKEU,
